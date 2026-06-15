d23d775b71e24a090644117297f02fdca3de2df2
Thisari428-bit <ureshathisari@gmail.com>
2026-06-15 16:49:36 +0530

    feat: create Redux Toolkit store

---
+++ client/src/components/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import chatReducer from './slices/chatSlice';
+import messageReducer from './slices/messageSlice';
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        chat: chatReducer,
+        message: messageReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export default store;
